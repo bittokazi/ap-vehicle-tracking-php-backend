0755934cb1df0beccaf6dbaa20cb93e3a164abc4
Bitto Kazi <bitto.kazi@gmail.com>
2019-05-29 12:48:19 +0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-define('HOME_URL', 'http://localhost');
-define('HOME_URL_SSL', 'https://localhost');
+define('HOME_URL', 'https://ap-vehicle-tracking.herokuapp.com');
+define('HOME_URL_SSL', 'https://ap-vehicle-tracking.herokuapp.com');
 
 define('ROOT_URL', dirname(__FILE__));
 define('DEBUG', true);
@@ -8,10 +8,10 @@ define('DEBUG', true);
 define('DB_TYPE', 'mysqli');
 define('CONNECT_DB', true);
 
-define('AUTH_KEY', md5('i1d2o3nt34345gi46346fuck'));
+define('AUTH_KEY', md5($_ENV['AUTH_KEY']));
 
-define('MYSQLI_DB_HOST', '0.0.0.0');
-define('MYSQLI_DB_USER', 'phpmyadmin');
-define('MYSQLI_DB_PASS', '1221');
-define('MYSQLI_DB_NAME', 'ap_vuhicle_track');
+define('MYSQLI_DB_HOST', $_ENV['DB_HOST']);
+define('MYSQLI_DB_USER', $_ENV['DB_USERNAME']);
+define('MYSQLI_DB_PASS', $_ENV['DB_PASSWORD']);
+define('MYSQLI_DB_NAME', $_ENV['DB_NAME']);
 ?>
